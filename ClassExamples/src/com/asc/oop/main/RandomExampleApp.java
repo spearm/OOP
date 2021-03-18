e7e8c8a804ad254c0482ed63fc425be5e93d9af6
spearm <55196222+spearm@users.noreply.github.com>
2021-03-18 12:18:00 -0400

    Init Project
    This is the import of the Class example from our local pc.

---
+++ ClassExamples/src/com/asc/oop/main/RandomExampleApp.java
@@ -0,0 +1,29 @@
+package com.asc.oop.main;
+
+import java.util.Scanner;
+
+public class RandomExampleApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to the Program \n Please enter your name in the following format (first last)");
+		String inpt 	=	"";
+		String fName 	=	"";
+		String lName 	=	""; 
+		
+		Scanner sc = new Scanner(System.in);
+		inpt = sc.nextLine();
+		// Mike Spear
+		fName = inpt.substring(0,4);
+		System.out.println("First name is:"+ fName);
+		
+		if() 
+		{
+		   
+				
+		}
+		
+
+	}
+
+}
